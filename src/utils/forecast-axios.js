89401e3013536601fd04a5933e3c81d2cfdf3229
Paul Baur <paul.baur@gmail.com>
2020-03-20 01:53:43 -0500

    Added high and low temperatures to weather report

--- src/utils/forecast-axios.js
+++ src/utils/forecast-axios.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
     if (data.error) {
       callback('Unable to find location.', undefined);
     } else {
-      const returnString =`${data.daily.data[0].summary} It is currently ${data.currently.temperature} degrees out. There is a ${data.currently.precipProbability * 100}% chance of rain.`;
+      const returnString =`${data.daily.data[0].summary} Today's high temperature is ${data.daily.data[0].temperatureHigh} degrees (F). Today's low temperature is ${data.daily.data[0].temperatureLow} degrees (F). It is currently ${data.currently.temperature} degrees (F) out. There is a ${data.currently.precipProbability * 100}% chance of rain.`;
       callback(undefined, returnString);
     }
   })
